20ebe4e704b49500c2497ed318bc6bf05192d883
mkostin <maxim.kostin@akvelon.com>
2014-05-16 14:02:22 +0400

    Updated formatting only.

--- KinectCommonBridge/ComSmartPtr.h
+++ KinectCommonBridge/ComSmartPtr.h
@@ -26,139 +26,139 @@ template<class INTERFACE, const IID* piid = NULL>
 class ComSmartPtr
 {
 public:
-	ComSmartPtr() { m_Ptr = NULL; }
-	
-	//TODO: try 'explicit'
-	ComSmartPtr(INTERFACE* lPtr)
-	{
-		m_Ptr = NULL;
+    ComSmartPtr() { m_Ptr = NULL; }
 
-		if (lPtr != NULL)
-		{
-			m_Ptr = lPtr;
-			m_Ptr->AddRef();
-		}
-	}
+    //TODO: try 'explicit'
+    ComSmartPtr(INTERFACE* lPtr)
+    {
+        m_Ptr = NULL;
 
-	ComSmartPtr(const ComSmartPtr<INTERFACE, piid>& refComPtr)
-	{
-		m_Ptr = NULL;
+        if (lPtr != NULL)
+        {
+            m_Ptr = lPtr;
+            m_Ptr->AddRef();
+        }
+    }
 
-		// using static cast here since conversion operator is explicitly defined
-		m_Ptr = static_cast<INTERFACE*>(refComPtr);
+    ComSmartPtr(const ComSmartPtr<INTERFACE, piid>& refComPtr)
+    {
+        m_Ptr = NULL;
 
-		if (m_Ptr)
-		{
-			m_Ptr->AddRef();
-		}
-	}
+        // using static cast here since conversion operator is explicitly defined
+        m_Ptr = static_cast<INTERFACE*>(refComPtr);
 
-	ComSmartPtr(IUnknown* pIUnknown, IID iid)
-	{
-		m_Ptr = NULL;
+        if (m_Ptr)
+        {
+            m_Ptr->AddRef();
+        }
+    }
 
-		if (pIUnknown != NULL)
-		{
-			//TODO: reinterpret_cast
-			pIUnknown->QueryInterface(iid, (void**)&m_Ptr);
-		}
-	}
+    ComSmartPtr(IUnknown* pIUnknown, IID iid)
+    {
+        m_Ptr = NULL;
 
-	~ComSmartPtr()
-	{
-		if (m_Ptr)
-		{
-			m_Ptr->Release();
-			m_Ptr = NULL;
-		}
-	}
+        if (pIUnknown != NULL)
+        {
+            //TODO: reinterpret_cast
+            pIUnknown->QueryInterface(iid, (void**)&m_Ptr);
+        }
+    }
+
+    ~ComSmartPtr()
+    {
+        if (m_Ptr)
+        {
+            m_Ptr->Release();
+            m_Ptr = NULL;
+        }
+    }
 
 public:
-	operator INTERFACE*() const
+    operator INTERFACE*() const
     {
-		// no assert() here since this will be called on e.g.:
-		// if (nullptr == p_comSmartPtrInstance) { ... }
-		// m_Ptr can be null and it'll be perfectly valid case.
+        // no assert() here since this will be called on e.g.:
+        // if (nullptr == p_comSmartPtrInstance) { ... }
+        // m_Ptr can be null and it'll be perfectly valid case.
         return m_Ptr;
     }
 
-	INTERFACE& operator*() const
+    INTERFACE& operator*() const
     {
         assert(m_Ptr != NULL);
-		return *m_Ptr;
+        return *m_Ptr;
     }
 
-	INTERFACE** operator&()
+    INTERFACE** operator&()
     {
         //assert(m_Ptr != NULL);
         return &m_Ptr;
     }
 
-	INTERFACE* operator->() const
-	{
-		assert(m_Ptr != NULL);
-		return m_Ptr;
-	}
-
-	bool operator==(const INTERFACE& lPtr) const
-	{
-		assert(m_Ptr != NULL);
-		return m_Ptr == lPtr;
-	}
-
-	bool operator!=(const INTERFACE& lPtr) const
-	{
-		assert(m_Ptr != NULL);
-		return !(m_Ptr == lPtr);
-	}
-
-	INTERFACE* operator=(INTERFACE* lPtr)
-	{
-		assert(lPtr != NULL);
-		if (IsEqualObject(lPtr))
-		{
-			return m_Ptr;
-		}
-		m_Ptr->Release();
-		lPtr->AddRef();
-		m_Ptr = lPtr;
-		return m_Ptr;
-	}
-
-	INTERFACE* operator=(IUnknown* pIUnknown)
+    INTERFACE* operator->() const
     {
-		assert(pIUnknown != NULL);
-		assert(piid != NULL);
-		//TODO: reinterpret_cast
-        pIUnknown->QueryInterface(*piid, (void**) &m_Ptr);
-		assert(m_Ptr != NULL);
-		return m_Ptr;
+        assert(m_Ptr != NULL);
+        return m_Ptr;
     }
 
-	INTERFACE* operator=(const ComSmartPtr<INTERFACE, piid>& RefComPtr)
-	{
-		assert(&RefComPtr != NULL);
-		// using static cast here since conversion operator is explicitly defined
-		m_Ptr = static_cast<INTERFACE*>(RefComPtr);
+    bool operator==(const INTERFACE& lPtr) const
+    {
+        assert(m_Ptr != NULL);
+        return m_Ptr == lPtr;
+    }
 
-		if (m_Ptr)
-		{
-			m_Ptr->AddRef();
-		}
-		return m_Ptr;
-	}
+    bool operator!=(const INTERFACE& lPtr) const
+    {
+        assert(m_Ptr != NULL);
+        return !(m_Ptr == lPtr);
+    }
 
-	void Attach(INTERFACE* lPtr)
+    INTERFACE* operator=(INTERFACE* lPtr)
+    {
+        assert(lPtr != NULL);
+        if (IsEqualObject(lPtr))
+        {
+            return m_Ptr;
+        }
+        m_Ptr->Release();
+        lPtr->AddRef();
+        m_Ptr = lPtr;
+        return m_Ptr;
+    }
+
+    INTERFACE* operator=(IUnknown* pIUnknown)
+    {
+        assert(pIUnknown != NULL);
+        assert(piid != NULL);
+        //TODO: reinterpret_cast
+        pIUnknown->QueryInterface(*piid, (void**) &m_Ptr);
+        assert(m_Ptr != NULL);
+        return m_Ptr;
+    }
+
+    INTERFACE* operator=(const ComSmartPtr<INTERFACE, piid>& RefComPtr)
+    {
+        assert(&RefComPtr != NULL);
+        // using static cast here since conversion operator is explicitly defined
+        m_Ptr = static_cast<INTERFACE*>(RefComPtr);
+
+        if (m_Ptr)
+        {
+            m_Ptr->AddRef();
+        }
+        return m_Ptr;
+    }
+
+    void Attach(INTERFACE* lPtr)
     {
         if (lPtr)
-		{
-			if (m_Ptr)
-			{
-				m_Ptr->Release();
-				m_Ptr = NULL;
-			}
-			m_Ptr = lPtr;
-		}
+        {
+            if (m_Ptr)
+            {
+                m_Ptr->Release();
+                m_Ptr = NULL;
+            }
+            m_Ptr = lPtr;
+        }
     }
 
     INTERFACE* Detach()
@@ -168,28 +168,28 @@ public:
         return lPtr;
     }
 
-	void Release()
-	{
-		if (m_Ptr)
-		{
-			m_Ptr->Release();
-			m_Ptr = NULL;
-		}
-	}
+    void Release()
+    {
+        if (m_Ptr)
+        {
+            m_Ptr->Release();
+            m_Ptr = NULL;
+        }
+    }
 
-	bool IsEqualObject(IUnknown* pOther)
-	{
-		assert(pOther != NULL);
-		IUnknown* pUnknown = NULL;
-		//TODO: reinterpret_cast
-		m_Ptr->QueryInterface(IID_IUnknown, (void**) &pUnknown);
+    bool IsEqualObject(IUnknown* pOther)
+    {
+        assert(pOther != NULL);
+        IUnknown* pUnknown = NULL;
+        //TODO: reinterpret_cast
+        m_Ptr->QueryInterface(IID_IUnknown, (void**) &pUnknown);
         bool result = (pOther == pUnknown) ? true : false;
-		pUnknown->Release();
-		return result;
-	}
+        pUnknown->Release();
+        return result;
+    }
 
 private:
-	INTERFACE* m_Ptr;
+    INTERFACE* m_Ptr;
 };
 
 #endif // _COMSMARTPTR_H_
