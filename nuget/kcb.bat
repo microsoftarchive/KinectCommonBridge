b39da25b2ab13c3e3cf096e0a9e8df4511b287d6
Neil McLaughlin <v-nemcla@microsoft.com>
2014-06-18 22:44:46 -0500

    Moved bat

--- nuget/kcb.bat
+++ nuget/kcb.bat
@@ -2,10 +2,10 @@ ren KinectCommonBridge KinectCommonBridge%1
 git clone https://github.com/MSOpenTech/KinectCommonBridge
 cd KinectCommonBridge 
 git checkout nuget
-call buildkcb2012
-call buildkcb2013
-call powershell -command "Write-NuGetPackage kcb.autopkg"
-call powershell -command "Write-NuGetPackage kcbx64.autopkg"
+call nuget\buildkcb2012
+call nuget\buildkcb2013
+call powershell -command "Write-NuGetPackage nuget\kcb.autopkg"
+call powershell -command "Write-NuGetPackage nuget\kcbx64.autopkg"
 call nuget\updateversion
 ..\nuget push *.nupkg
 
