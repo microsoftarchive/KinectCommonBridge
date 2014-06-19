a4bb428192e8a9bbe9e8a61da8e5e2bc6c1ad106
mkostin <maxim.kostin@akvelon.com>
2014-06-19 17:14:59 +0400

    Added InfraredBasics 2.0 sample.

---
+++ examples/InfraredBasics-KCB/InfraredBasics.cpp
@@ -0,0 +1,537 @@
+//------------------------------------------------------------------------------
+// <copyright file="InfraredBasics.cpp" company="Microsoft">
+//     The Kinect for Windows APIs used here are preliminary and subject to change
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include <strsafe.h>
+#include "resource.h"
+#include "InfraredBasics.h"
+
+/// <summary>
+/// Entry point for the application
+/// </summary>
+/// <param name="hInstance">handle to the application instance</param>
+/// <param name="hPrevInstance">always 0</param>
+/// <param name="lpCmdLine">command line arguments</param>
+/// <param name="nCmdShow">whether to display minimized, maximized, or normally</param>
+/// <returns>status</returns>
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    CInfraredBasics application;
+    application.Run(hInstance, nCmdShow);
+}
+
+/// <summary>
+/// Constructor
+/// </summary>
+CInfraredBasics::CInfraredBasics() :
+    m_hWnd(NULL),
+    m_nStartTime(0),
+    m_nLastCounter(0),
+    m_nFramesSinceUpdate(0),
+    m_fFreq(0),
+    m_nNextStatusTime(0),
+    m_bSaveScreenshot(false),
+    m_hKCB(KCB_INVALID_HANDLE),
+    m_pD2DFactory(NULL),
+    m_pDrawInfrared(NULL),
+    m_pInfraredRGBX(NULL)
+{
+    LARGE_INTEGER qpf = {0};
+    if (QueryPerformanceFrequency(&qpf))
+    {
+        m_fFreq = double(qpf.QuadPart);
+    }
+
+    // create heap storage for infrared pixel data in RGBX format
+    m_pInfraredRGBX = new RGBQUAD[cInfraredWidth * cInfraredHeight];
+}
+  
+
+/// <summary>
+/// Destructor
+/// </summary>
+CInfraredBasics::~CInfraredBasics()
+{
+    // clean up Direct2D renderer
+    if (m_pDrawInfrared)
+    {
+        delete m_pDrawInfrared;
+        m_pDrawInfrared = NULL;
+    }
+
+    if (m_pInfraredRGBX)
+    {
+        delete [] m_pInfraredRGBX;
+        m_pInfraredRGBX = NULL;
+    }
+
+    // clean up Direct2D
+    SafeRelease(m_pD2DFactory);
+
+    // delete the buffer
+    SafeArrayDelete(m_kcbInfraredFrame.Buffer);
+
+    // close the handle
+    if (KCB_INVALID_HANDLE != m_hKCB)
+    {
+        KCBCloseSensor(&m_hKCB);
+    }
+}
+
+/// <summary>
+/// Creates the main window and begins processing
+/// </summary>
+/// <param name="hInstance">handle to the application instance</param>
+/// <param name="nCmdShow">whether to display minimized, maximized, or normally</param>
+int CInfraredBasics::Run(HINSTANCE hInstance, int nCmdShow)
+{
+    MSG       msg = {0};
+    WNDCLASS  wc;
+
+    // Dialog custom window class
+    ZeroMemory(&wc, sizeof(wc));
+    wc.style         = CS_HREDRAW | CS_VREDRAW;
+    wc.cbWndExtra    = DLGWINDOWEXTRA;
+    wc.hCursor       = LoadCursorW(NULL, IDC_ARROW);
+    wc.hIcon         = LoadIconW(hInstance, MAKEINTRESOURCE(IDI_APP));
+    wc.lpfnWndProc   = DefDlgProcW;
+    wc.lpszClassName = L"InfraredBasicsAppDlgWndClass";
+
+    if (!RegisterClassW(&wc))
+    {
+        return 0;
+    }
+
+    // Create main application window
+    HWND hWndApp = CreateDialogParamW(
+        NULL,
+        MAKEINTRESOURCE(IDD_APP),
+        NULL,
+        (DLGPROC)CInfraredBasics::MessageRouter, 
+        reinterpret_cast<LPARAM>(this));
+
+    // Show window
+    ShowWindow(hWndApp, nCmdShow);
+
+    // Main message loop
+    while (WM_QUIT != msg.message)
+    {
+        Update();
+
+        while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            // If a dialog message will be taken care of by the dialog proc
+            if (hWndApp && IsDialogMessageW(hWndApp, &msg))
+            {
+                continue;
+            }
+
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
+    return static_cast<int>(msg.wParam);
+}
+
+/// <summary>
+/// Main processing function
+/// </summary>
+void CInfraredBasics::Update()
+{
+    if (KCB_INVALID_HANDLE == m_hKCB)
+    {
+        return;
+    }
+
+    //IInfraredFrame* pInfraredFrame = NULL;
+
+    HRESULT hr = KCBGetInfraredFrame(m_hKCB, &m_kcbInfraredFrame);
+
+    if (SUCCEEDED(hr))
+    {
+        //INT64 nTime = 0;
+        //IFrameDescription* pFrameDescription = NULL;
+        //int nWidth = 0;
+        //int nHeight = 0;
+        //UINT nBufferSize = 0;
+        //UINT16 *pBuffer = NULL;
+
+        //hr = pInfraredFrame->get_RelativeTime(&nTime);
+
+        //if (SUCCEEDED(hr))
+        //{
+        //    hr = pInfraredFrame->get_FrameDescription(&pFrameDescription);
+        //}
+
+        //if (SUCCEEDED(hr))
+        //{
+        //    hr = pFrameDescription->get_Width(&nWidth);
+        //}
+
+        //if (SUCCEEDED(hr))
+        //{
+        //    hr = pFrameDescription->get_Height(&nHeight);
+        //}
+
+        //if (SUCCEEDED(hr))
+        //{
+        //    hr = pInfraredFrame->AccessUnderlyingBuffer(&nBufferSize, &pBuffer);            
+        //}
+
+        if (SUCCEEDED(hr))
+        {
+            ProcessInfrared(m_kcbInfraredFrame.TimeStamp, m_kcbInfraredFrame.Buffer, cInfraredWidth, cInfraredHeight);
+        }
+
+        //SafeRelease(pFrameDescription);
+    }
+
+    //SafeRelease(pInfraredFrame);
+}
+
+/// <summary>
+/// Handles window messages, passes most to the class instance to handle
+/// </summary>
+/// <param name="hWnd">window message is for</param>
+/// <param name="uMsg">message</param>
+/// <param name="wParam">message data</param>
+/// <param name="lParam">additional message data</param>
+/// <returns>result of message processing</returns>
+LRESULT CALLBACK CInfraredBasics::MessageRouter(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    CInfraredBasics* pThis = NULL;
+    
+    if (WM_INITDIALOG == uMsg)
+    {
+        pThis = reinterpret_cast<CInfraredBasics*>(lParam);
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pThis));
+    }
+    else
+    {
+        pThis = reinterpret_cast<CInfraredBasics*>(::GetWindowLongPtr(hWnd, GWLP_USERDATA));
+    }
+
+    if (pThis)
+    {
+        return pThis->DlgProc(hWnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+/// <summary>
+/// Handle windows messages for the class instance
+/// </summary>
+/// <param name="hWnd">window message is for</param>
+/// <param name="uMsg">message</param>
+/// <param name="wParam">message data</param>
+/// <param name="lParam">additional message data</param>
+/// <returns>result of message processing</returns>
+LRESULT CALLBACK CInfraredBasics::DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(wParam);
+    UNREFERENCED_PARAMETER(lParam);
+
+    switch (message)
+    {
+        case WM_INITDIALOG:
+        {
+            // Bind application window handle
+            m_hWnd = hWnd;
+
+            // Init Direct2D
+            D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pD2DFactory);
+
+            // Create and initialize a new Direct2D image renderer (take a look at ImageRenderer.h)
+            // We'll use this to draw the data we receive from the Kinect to the screen
+            m_pDrawInfrared = new ImageRenderer();
+            HRESULT hr = m_pDrawInfrared->Initialize(GetDlgItem(m_hWnd, IDC_VIDEOVIEW), m_pD2DFactory, cInfraredWidth, cInfraredHeight, cInfraredWidth * sizeof(RGBQUAD)); 
+            if (FAILED(hr))
+            {
+                SetStatusMessage(L"Failed to initialize the Direct2D draw device.", 10000, true);
+            }
+
+            // Get and initialize the default Kinect sensor
+            InitializeDefaultSensor();
+        }
+        break;
+
+        // If the titlebar X is clicked, destroy app
+        case WM_CLOSE:
+            DestroyWindow(hWnd);
+            break;
+
+        case WM_DESTROY:
+            // Quit the main message pump
+            PostQuitMessage(0);
+            break;
+
+        // Handle button press
+        case WM_COMMAND:
+            // If it was for the screenshot control and a button clicked event, save a screenshot next frame 
+            if (IDC_BUTTON_SCREENSHOT == LOWORD(wParam) && BN_CLICKED == HIWORD(wParam))
+            {
+                m_bSaveScreenshot = true;
+            }
+            break;
+    }
+
+    return FALSE;
+}
+
+/// <summary>
+/// Initializes the default Kinect sensor
+/// </summary>
+/// <returns>indicates success or failure</returns>
+HRESULT CInfraredBasics::InitializeDefaultSensor()
+{
+    HRESULT hr = S_OK;
+
+    m_hKCB = KCBOpenDefaultSensor();
+    if (KCB_INVALID_HANDLE == m_hKCB)
+    {
+        hr = E_UNEXPECTED;
+    }
+
+    // create re-usable buffer
+    KCBFrameDescription frameDesc;
+    if (SUCCEEDED(hr))
+    {
+        hr = KCBGetInfraredFrameDescription(m_hKCB, &frameDesc);
+        if (SUCCEEDED(hr))
+        {
+            m_kcbInfraredFrame.Size = frameDesc.lengthInPixels;
+            m_kcbInfraredFrame.Buffer = new UINT16[m_kcbInfraredFrame.Size];
+        }
+    }
+
+    if (FAILED(hr))
+    {
+        SetStatusMessage(L"No ready Kinect found!", 10000, true);
+        return E_FAIL;
+    }
+
+    return hr;
+}
+
+/// <summary>
+/// Handle new infrared data
+/// <param name="nTime">timestamp of frame</param>
+/// <param name="pBuffer">pointer to frame data</param>
+/// <param name="nWidth">width (in pixels) of input image data</param>
+/// <param name="nHeight">height (in pixels) of input image data</param>
+/// </summary>
+void CInfraredBasics::ProcessInfrared(INT64 nTime, const UINT16* pBuffer, int nWidth, int nHeight)
+{
+    if (m_hWnd)
+    {
+        if (!m_nStartTime)
+        {
+            m_nStartTime = nTime;
+        }
+
+        double fps = 0.0;
+
+        LARGE_INTEGER qpcNow = {0};
+        if (m_fFreq)
+        {
+            if (QueryPerformanceCounter(&qpcNow))
+            {
+                if (m_nLastCounter)
+                {
+                    m_nFramesSinceUpdate++;
+                    fps = m_fFreq * m_nFramesSinceUpdate / double(qpcNow.QuadPart - m_nLastCounter);
+                }
+            }
+        }
+
+        WCHAR szStatusMessage[64];
+        StringCchPrintf(szStatusMessage, _countof(szStatusMessage), L" FPS = %0.2f    Time = %I64d", fps, (nTime - m_nStartTime));
+
+        if (SetStatusMessage(szStatusMessage, 1000, false))
+        {
+            m_nLastCounter = qpcNow.QuadPart;
+            m_nFramesSinceUpdate = 0;
+        }
+    }
+
+    if (m_pInfraredRGBX && pBuffer && (nWidth == cInfraredWidth) && (nHeight == cInfraredHeight))
+    {
+        RGBQUAD* pRGBX = m_pInfraredRGBX;
+
+        // end pixel is start + width*height - 1
+        const UINT16* pBufferEnd = pBuffer + (nWidth * nHeight);
+
+        while (pBuffer < pBufferEnd)
+        {
+            USHORT ir = *pBuffer;
+
+            // To convert to a byte, we're discarding the least-significant bits.
+            BYTE intensity = static_cast<BYTE>(ir >> 8);
+
+            pRGBX->rgbRed   = intensity;
+            pRGBX->rgbGreen = intensity;
+            pRGBX->rgbBlue  = intensity;
+
+            ++pRGBX;
+            ++pBuffer;
+        }
+
+        // Draw the data with Direct2D
+        m_pDrawInfrared->Draw(reinterpret_cast<BYTE*>(m_pInfraredRGBX), cInfraredWidth * cInfraredHeight * sizeof(RGBQUAD));
+
+        if (m_bSaveScreenshot)
+        {
+            WCHAR szScreenshotPath[MAX_PATH];
+
+            // Retrieve the path to My Photos
+            GetScreenshotFileName(szScreenshotPath, _countof(szScreenshotPath));
+
+            // Write out the bitmap to disk
+            HRESULT hr = SaveBitmapToFile(reinterpret_cast<BYTE*>(m_pInfraredRGBX), nWidth, nHeight, sizeof(RGBQUAD) * 8, szScreenshotPath);
+
+            WCHAR szStatusMessage[64 + MAX_PATH];
+            if (SUCCEEDED(hr))
+            {
+                // Set the status bar to show where the screenshot was saved
+                StringCchPrintf(szStatusMessage, _countof(szStatusMessage), L"Screenshot saved to %s", szScreenshotPath);
+            }
+            else
+            {
+                StringCchPrintf(szStatusMessage, _countof(szStatusMessage), L"Failed to write screenshot to %s", szScreenshotPath);
+            }
+
+            SetStatusMessage(szStatusMessage, 5000, true);
+
+            // toggle off so we don't save a screenshot again next frame
+            m_bSaveScreenshot = false;
+        }
+    }
+}
+
+/// <summary>
+/// Set the status bar message
+/// </summary>
+/// <param name="szMessage">message to display</param>
+/// <param name="showTimeMsec">time in milliseconds to ignore future status messages</param>
+/// <param name="bForce">force status update</param>
+bool CInfraredBasics::SetStatusMessage(WCHAR* szMessage, DWORD nShowTimeMsec, bool bForce)
+{
+    DWORD now = GetTickCount();
+
+    if (m_hWnd && (bForce || (m_nNextStatusTime <= now)))
+    {
+        SetDlgItemText(m_hWnd, IDC_STATUS, szMessage);
+        m_nNextStatusTime = now + nShowTimeMsec;
+
+        return true;
+    }
+
+    return false;
+}
+
+/// <summary>
+/// Get the name of the file where screenshot will be stored.
+/// </summary>
+/// <param name="lpszFilePath">string buffer that will receive screenshot file name.</param>
+/// <param name="nFilePathSize">number of characters in lpszFilePath string buffer.</param>
+/// <returns>
+/// S_OK on success, otherwise failure code.
+/// </returns>
+HRESULT CInfraredBasics::GetScreenshotFileName(LPWSTR lpszFilePath, UINT nFilePathSize)
+{
+    WCHAR* pszKnownPath = NULL;
+    HRESULT hr = SHGetKnownFolderPath(FOLDERID_Pictures, 0, NULL, &pszKnownPath);
+
+    if (SUCCEEDED(hr))
+    {
+        // Get the time
+        WCHAR szTimeString[MAX_PATH];
+        GetTimeFormatEx(NULL, 0, NULL, L"hh'-'mm'-'ss", szTimeString, _countof(szTimeString));
+
+        // File name will be KinectScreenshotInfrared-HH-MM-SS.bmp
+        StringCchPrintfW(lpszFilePath, nFilePathSize, L"%s\\KinectScreenshot-Infrared-%s.bmp", pszKnownPath, szTimeString);
+    }
+
+    if (pszKnownPath)
+    {
+        CoTaskMemFree(pszKnownPath);
+    }
+
+    return hr;
+}
+
+/// <summary>
+/// Save passed in image data to disk as a bitmap
+/// </summary>
+/// <param name="pBitmapBits">image data to save</param>
+/// <param name="lWidth">width (in pixels) of input image data</param>
+/// <param name="lHeight">height (in pixels) of input image data</param>
+/// <param name="wBitsPerPixel">bits per pixel of image data</param>
+/// <param name="lpszFilePath">full file path to output bitmap to</param>
+/// <returns>indicates success or failure</returns>
+HRESULT CInfraredBasics::SaveBitmapToFile(BYTE* pBitmapBits, LONG lWidth, LONG lHeight, WORD wBitsPerPixel, LPCWSTR lpszFilePath)
+{
+    DWORD dwByteCount = lWidth * lHeight * (wBitsPerPixel / 8);
+
+    BITMAPINFOHEADER bmpInfoHeader = {0};
+
+    bmpInfoHeader.biSize        = sizeof(BITMAPINFOHEADER);  // Size of the header
+    bmpInfoHeader.biBitCount    = wBitsPerPixel;             // Bit count
+    bmpInfoHeader.biCompression = BI_RGB;                    // Standard RGB, no compression
+    bmpInfoHeader.biWidth       = lWidth;                    // Width in pixels
+    bmpInfoHeader.biHeight      = -lHeight;                  // Height in pixels, negative indicates it's stored right-side-up
+    bmpInfoHeader.biPlanes      = 1;                         // Default
+    bmpInfoHeader.biSizeImage   = dwByteCount;               // Image size in bytes
+
+    BITMAPFILEHEADER bfh = {0};
+
+    bfh.bfType    = 0x4D42;                                           // 'M''B', indicates bitmap
+    bfh.bfOffBits = bmpInfoHeader.biSize + sizeof(BITMAPFILEHEADER);  // Offset to the start of pixel data
+    bfh.bfSize    = bfh.bfOffBits + bmpInfoHeader.biSizeImage;        // Size of image + headers
+
+    // Create the file on disk to write to
+    HANDLE hFile = CreateFileW(lpszFilePath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    // Return if error opening file
+    if (NULL == hFile) 
+    {
+        return E_ACCESSDENIED;
+    }
+
+    DWORD dwBytesWritten = 0;
+    
+    // Write the bitmap file header
+    if (!WriteFile(hFile, &bfh, sizeof(bfh), &dwBytesWritten, NULL))
+    {
+        CloseHandle(hFile);
+        return E_FAIL;
+    }
+    
+    // Write the bitmap info header
+    if (!WriteFile(hFile, &bmpInfoHeader, sizeof(bmpInfoHeader), &dwBytesWritten, NULL))
+    {
+        CloseHandle(hFile);
+        return E_FAIL;
+    }
+    
+    // Write the RGB Data
+    if (!WriteFile(hFile, pBitmapBits, bmpInfoHeader.biSizeImage, &dwBytesWritten, NULL))
+    {
+        CloseHandle(hFile);
+        return E_FAIL;
+    }    
+
+    // Close the file
+    CloseHandle(hFile);
+    return S_OK;
+}
+
---
+++ examples/InfraredBasics-KCB/InfraredBasics.h
@@ -0,0 +1,123 @@
+//------------------------------------------------------------------------------
+// <copyright file="InfraredBasics.h" company="Microsoft">
+//     The Kinect for Windows APIs used here are preliminary and subject to change
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "resource.h"
+#include "ImageRenderer.h"
+
+class CInfraredBasics
+{
+    static const int        cInfraredWidth  = 512;
+    static const int        cInfraredHeight = 424;
+
+public:
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    CInfraredBasics();
+
+    /// <summary>
+    /// Destructor
+    /// </summary>
+    ~CInfraredBasics();
+
+    /// <summary>
+    /// Handles window messages, passes most to the class instance to handle
+    /// </summary>
+    /// <param name="hWnd">window message is for</param>
+    /// <param name="uMsg">message</param>
+    /// <param name="wParam">message data</param>
+    /// <param name="lParam">additional message data</param>
+    /// <returns>result of message processing</returns>
+    static LRESULT CALLBACK MessageRouter(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    /// <summary>
+    /// Handle windows messages for a class instance
+    /// </summary>
+    /// <param name="hWnd">window message is for</param>
+    /// <param name="uMsg">message</param>
+    /// <param name="wParam">message data</param>
+    /// <param name="lParam">additional message data</param>
+    /// <returns>result of message processing</returns>
+    LRESULT CALLBACK        DlgProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    /// <summary>
+    /// Creates the main window and begins processing
+    /// </summary>
+    /// <param name="hInstance"></param>
+    /// <param name="nCmdShow"></param>
+    int                     Run(HINSTANCE hInstance, int nCmdShow);
+
+private:
+    HWND                    m_hWnd;
+    INT64                   m_nStartTime;
+    INT64                   m_nLastCounter;
+    DWORD                   m_nFramesSinceUpdate;
+    double                  m_fFreq;
+    DWORD                   m_nNextStatusTime;
+    bool                    m_bSaveScreenshot;
+
+    // KCB Handle for Kinect v2
+    KCBHANDLE				m_hKCB;
+    KCBInfraredFrame        m_kcbInfraredFrame;
+
+    // Direct2D
+    ImageRenderer*          m_pDrawInfrared;
+    ID2D1Factory*           m_pD2DFactory;
+    RGBQUAD*                m_pInfraredRGBX;
+
+    /// <summary>
+    /// Main processing function
+    /// </summary>
+    void                    Update();
+
+    /// <summary>
+    /// Initializes the default Kinect sensor
+    /// </summary>
+    /// <returns>S_OK on success, otherwise failure code</returns>
+    HRESULT                 InitializeDefaultSensor();
+
+    /// <summary>
+    /// Handle new infrared data
+    /// <param name="nTime">timestamp of frame</param>
+    /// <param name="pBuffer">pointer to frame data</param>
+    /// <param name="nWidth">width (in pixels) of input image data</param>
+    /// <param name="nHeight">height (in pixels) of input image data</param>
+    /// </summary>
+    void                    ProcessInfrared(INT64 nTime, const UINT16* pBuffer, int nHeight, int nWidth);
+
+    /// <summary>
+    /// Set the status bar message
+    /// </summary>
+    /// <param name="szMessage">message to display</param>
+    /// <param name="nShowTimeMsec">time in milliseconds for which to ignore future status messages</param>
+    /// <param name="bForce">force status update</param>
+    bool                    SetStatusMessage(WCHAR* szMessage, DWORD nShowTimeMsec, bool bForce);
+
+    /// <summary>
+    /// Get the name of the file where screenshot will be stored.
+    /// </summary>
+    /// <param name="lpszFilePath">string buffer that will receive screenshot file name.</param>
+    /// <param name="nFilePathSize">number of characters in lpszFilePath string buffer.</param>
+    /// <returns>
+    /// S_OK on success, otherwise failure code.
+    /// </returns>
+    HRESULT                 GetScreenshotFileName(LPWSTR lpszFilePath, UINT nFilePathSize);
+
+    /// <summary>
+    /// Save passed in image data to disk as a bitmap
+    /// </summary>
+    /// <param name="pBitmapBits">image data to save</param>
+    /// <param name="lWidth">width (in pixels) of input image data</param>
+    /// <param name="lHeight">height (in pixels) of input image data</param>
+    /// <param name="wBitsPerPixel">bits per pixel of image data</param>
+    /// <param name="lpszFilePath">full file path to output bitmap to</param>
+    /// <returns>indicates success or failure</returns>
+    HRESULT                 SaveBitmapToFile(BYTE* pBitmapBits, LONG lWidth, LONG lHeight, WORD wBitsPerPixel, LPCWSTR lpszFilePath);
+};
+
