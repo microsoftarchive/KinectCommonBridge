84702aa00ec7a61ab1e8a462b4fc05d1b010a877
Neil McLaughlin <v-nemcla@microsoft.com>
2014-06-21 09:41:17 -0500

    Handling x64

--- nuget/kcb.bat
+++ nuget/kcb.bat
@@ -5,7 +5,7 @@ git checkout nuget
 call nuget\buildkcb2012
 call nuget\buildkcb2013
 call nuget\cdnuget
-call updateversion kcb.autopkg
+call updateversion kcb.autopkg kcbx64.autopkg
 call powershell -command "Write-NuGetPackage kcb.autopkg"
 call powershell -command "Write-NuGetPackage kcbx64.autopkg"
 call nugetpublish
--- nuget/updateversion.bat
+++ nuget/updateversion.bat
@@ -1,4 +1,4 @@
 call nuget\cdnuget
 call buildutil
 call runutil %1
-
+call runutil %2
