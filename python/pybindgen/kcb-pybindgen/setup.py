6bab49ef0a58acd67a6502bf3289c2d015fc5cf2
mkostin <maxim.kostin@akvelon.com>
2014-09-03 17:38:49 +0400

    Updated includes, libs and compiler options. Compilation and linking are working now.

--- python/pybindgen/kcb-pybindgen/setup.py
+++ python/pybindgen/kcb-pybindgen/setup.py
@@ -4,41 +4,80 @@ import os
 from distutils.core import setup, Extension
 from interface import generate_module
 
-LIB = "d:/projects/KinectCB/KinectCommonBridge-v2/Release"
+# Path to Kinect library (.lib)
+LIB = ["c:/Program Files/Microsoft SDKs/Kinect/v2.0-PublicPreview1407/Lib/x86"]
 
+# Kinect includes path
 INC_KINECT = ["C:/Program Files/Microsoft SDKs/Kinect/v2.0-PublicPreview1407/inc"]
 
+# Windows includes used by VS 2012
 INC_2012 = [
     "C:/Program Files (x86)/Windows Kits/8.0/Include/um",
     "C:/Program Files (x86)/Windows Kits/8.0/Include/shared",
     "C:/Program Files (x86)/Microsoft Visual Studio 11.0/VC/include"
 ]
 
+# Windows includes used by VS 2010
 INC_2010 = [
-    #"C:/Program Files (x86)/Windows Kits/v7.0a/Include/um",
-    #"C:/Program Files (x86)/Windows Kits/v7.0a/Include/shared",
-    "C:/Program Files (x86)/Windows Kits/8.1/Include/shared"
-    #"D:/dev/msvc/2010/VC/include"
+    # "C:/Program Files (x86)/Windows Kits/v7.0a/Include/um",
+    "C:/Program Files (x86)/Windows Kits/v7.0a/Include/shared",  # can be commented out, however VS makes use of it
+    "C:/Program Files (x86)/Windows Kits/8.1/Include/shared",
+    "D:/dev/msvc/2010/VC/include"
 ]
 
-#  Set of includes that is going to be used
+# Set of includes that is going to be used
 INC = ["."] + INC_KINECT + INC_2010
 
-MACROS = [# these will make 'KINECT_CB' equal '__declspec(dllexport)'
-          ('KCBV2_EXPORTS', None), ("KCBV2LIB_EXPORTS", None),
-          # others are taken from Visual Studio 'cl.exe' execution log
-          ("WIN32", None), ("NDEBUG", None), ("_WINDOWS", None),
-          ("_USRDLL", None), ("_WINDLL", None), ("_UNICODE", None), ("UNICODE", None)]
+# Set of macro definitions required for proper compilation
+MACROS = [('KCBV2_EXPORTS', None), ("KCBV2LIB_EXPORTS", None),  # will make 'KINECT_CB' equal '__declspec(dllexport)'
+          ("NDEBUG", None),  # making release build
+          ("WIN32", None), ("_WINDOWS", None), ("_USRDLL", None),  # taken from Visual Studio 'cl.exe' execution log
+          ("_WINDLL", None), ("_UNICODE", None), ("UNICODE", None)]
 
-EXTRA_COMPILE_ARGS_MACRO = ["/D WIN32", "/D NDEBUG", "/D _WINDOWS", "/D _USRDLL", "/D KCBV2LIB_EXPORTS", "/D _WINDLL", "/D _UNICODE", "/D UNICODE"]
+# NOT USED. The same set of macro definitions as 'MACROS' but prepared to be provided as compile arguments
+EXTRA_COMPILE_ARGS_MACRO = ["/D WIN32", "/D NDEBUG", "/D _WINDOWS", "/D _USRDLL", "/D KCBV2LIB_EXPORTS", "/D _WINDLL",
+                            "/D _UNICODE", "/D UNICODE"]
 
-EXTRA_COMPILE_ARGS = ["/EHsc", "/Zi", "/nologo", "/W3", "/WX-", "/O2", "/Oi", "/Oy-",
-                      "/GL", "/Gm-", "/EHsc", "/MD", "/GS-", "/Gy",
-                      "/fp:precise", "/Zc:wchar_t", "/Zc:forScope",
+# See:
+# - "http://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx"
+# - "http://msdn.microsoft.com/en-us/library/610ecb4h.aspx"
+EXTRA_COMPILE_ARGS = ["/EHsc",  # catch only c++ exceptions
+                      "/Zi",  # generates .pdb file
+                      "/nologo",  # suppress extra banners
+                      "/W3",  # Warning level >=2
+                      "/WX-",  # Don't treat compiler warnings as errors
+                      "/Od",   # Disable optimization
+                      "/Oy-",  # Disable function pointers omission
+                      "/Gm-",  # Disable minimal rebuild
+                      "/MD",  # Use multithreaded release runtime library
+                      "/GS-",  # Disable buffer security check
+                      "/Gy",  # Enables function-level linking
+                      "/fp:precise",  # Floating-point calculation mode
+                      "/Zc:wchar_t",  # Map 'wchar_t' to '__wchar_t'
+                      "/Zc:forScope",  # Dons't use for initializer outside of 'for' scope
                       #"/Fo\"Release\\", "/Fd\"Release\vc100.pdb",
-                      "/Gd", "/TP", "/analyze-", "/errorReport:prompt"
-] # + EXTRA_COMPILE_ARGS_MACRO
+                      "/Gd",  # Specifies __cdecl calling convention
+                      "/TP",  # File names are C++ source files, even w/o .cpp or .cxx extension
+                      "/analyze-",  # Disables code analysis
+                      "/errorReport:none",  # Disable reporting internal compiler errors
 
+                      # linker options
+                      "/link",
+                      "/OUT:\"D:\projects\KinectCB\KinectCommonBridge-v2-2010\KCBv2Lib\Release\KCBv2.dll\"",
+                      "/INCREMENTAL:NO",
+                      "/SUBSYSTEM:WINDOWS",
+                      "/OPT:REF",
+                      "/OPT:ICF",
+                      "/LTCG",
+                      "/TLBID:1",
+                      "/DYNAMICBASE",
+                      "/NXCOMPAT",
+                      "/MACHINE:X86",
+                      "/DLL"
+]  # + EXTRA_COMPILE_ARGS_MACRO
+
+# Source files included into compilation process
+SRC = ['stdafx.cpp', 'dllmain.cpp', 'KinectList.cpp', 'KCBFrames.cpp', 'KCBSensor.cpp', 'KCBv2Lib.cpp']
 
 try:
     os.mkdir("build")
@@ -49,29 +88,18 @@ with open(module_fname, "wt") as file_:
     print("Generating file {}".format(module_fname))
     generate_module(file_)
 
-# ext = Extension("_KCBv2Lib",
-# include_dirs = INC,
-# library_dirs = [LIB],
-# sources = ["KCBv2Lib.i", "KCBv2Lib.h"],
-# 	swig_opts=['-threads', '-c++'],
-# 	language = 'c++',
-#     extra_compile_args = ['/Zi', '/EHsc'],
-# )
-
-
-mymodule = Extension('KCBv2Lib',
-                     sources=[module_fname,  'stdafx.cpp', 'dllmain.cpp', 'KinectList.cpp', 'KCBFrames.cpp', 'KCBSensor.cpp', 'KCBv2Lib.cpp'],
+kcbmodule = Extension('KCBv2Lib',
+                     sources=[module_fname] + SRC,
                      include_dirs=INC,
-                     library_dirs=[LIB],
+                     library_dirs=LIB,
                      define_macros=MACROS,
                      extra_compile_args=EXTRA_COMPILE_ARGS
-                     #language='c++'
 )
 
 setup(name='KCBv2',
-      version="0.0",
+      version="0.1",
       description='KCBv2',
       author='Maxim Kostin',
       author_email='dev@maxkostin.ru',
-      ext_modules=[mymodule],
+      ext_modules=[kcbmodule],
 )
