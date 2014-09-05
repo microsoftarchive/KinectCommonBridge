a2c2f250e20e92a698262578a012c921b7714f15
mkostin <maxim.kostin@akvelon.com>
2014-09-05 17:05:53 +0400

    Updated paths for automatic generation script.

--- python/pybindgen/pybindgen-gccxml.py
+++ python/pybindgen/pybindgen-gccxml.py
@@ -6,12 +6,13 @@ import pybindgen
 from pybindgen import FileCodeSink
 from pybindgen.gccxmlparser import ModuleParser
 
+KCB_HEADERS = ["D:/projects/KinectCB/KinectCommonBridge-v2-python/KCBv2/KCBv2Lib.h"]
 
 def my_module_gen():
     module_parser = ModuleParser('KCBv2Lib', '::')
     module = module_parser.parse(
-        header_files=['d:\projects\KinectCB\KinectCommonBridge-python\KCBv2\KCBv2Lib.h'],
-        include_paths=['C:\Program Files\Microsoft SDKs\Kinect\v2.0-DevPreview1406\inc'])
+        header_files=KCB_HEADERS,
+        include_paths=['C:/Program Files/Microsoft SDKs/Kinect/v2.0-PublicPreview1407/inc'])
     #pygen_sink=[FileCodeSink('sink.py')]
     #module = module_parser.parse(sys.argv[1:])
     #module.add_include('"d:\projects\KinectCB\KinectCommonBridge-python\KCBv2\KCBv2Lib.h"')
